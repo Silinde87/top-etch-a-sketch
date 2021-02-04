05f662334765fcc5f8cd7be933d326b6ce7cc8a6
Silinde87 <paurodriguezmolina@gmail.com>
2021-02-04 18:12:09 +0100

    feat: Implement paintGrid() and mousemove on grid element; Closes #1
    Delete the border grid

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //JS
 
-let gridSize = 6;
+let gridSize = 16;
+const COLOR_BLACK = "black";
+
+let grid = document.getElementById('grid');
 
 //This function builds the grid with the size as a parameter.
 function buildGrid(size){
-    let grid = document.getElementById('grid');
-    let squareSize = document.getElementById('grid').clientWidth / size -1;
-
+    
+    let squareSize = document.getElementById('grid').clientWidth / size;
     //Creating the square and defining his size
     for(let i=1 ; i<=size*size;i++){ 
         let square = document.createElement('div')
@@ -15,27 +17,23 @@ function buildGrid(size){
         square.style.width = squareSize + "px";
         square.style.height = squareSize + "px";
     }
-
-    //Removing top border from grid
-    grid = Array.from(grid.getElementsByClassName('square-grid'));
-    for(let i=0 ; i<size ; i++){
-        grid[i].style.borderTop = "none";
-    }
-    //Removing left border from grid
-    for(let i=0 ; i<size*size;){
-        grid[i].style.borderLeft = "none";
-        i+=size;
-    }    
+ 
 }
 
 //this function paints the grid
-function paintGrid(){
-
+function paintGrid(elem, color){
+    let square = elem.target;    
+    square.style.backgroundColor = color
 }
 
 function eraseGrid(){
 
 }
 
-
+console.log(grid.offsetHeight)
+grid.addEventListener('mousemove', e=>{    
+    if(e.target.classList == "square-grid"){
+        paintGrid(e, COLOR_BLACK);
+    }    
+});
 buildGrid(gridSize);
