c364a667e2ec4151ca407479fa3be774210b724b
Silinde87 <paurodriguezmolina@gmail.com>
2021-02-04 19:16:29 +0100

    feat: Implement random color function.
    getRamdomRgb() and also eraseGrid() implemented. Eraso buttons trigger eraseGrid()

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 
 let gridSize = 16;
 const COLOR_BLACK = "black";
+let COLOR_RANDOM = getRandomRgb();
 
 let grid = document.getElementById('grid');
+let eraseButton = document.getElementById('reset-controller')
+
+buildGrid(gridSize);
 
 //This function builds the grid with the size as a parameter.
 function buildGrid(size){
@@ -16,6 +20,16 @@ function buildGrid(size){
         square.classList.add('square-grid');
         square.style.width = squareSize + "px";
         square.style.height = squareSize + "px";
+        //Rounding square grid corners
+        if(i==1){
+            square.style.borderTopLeftRadius = "10px";
+        }else if(i==size){
+            square.style.borderTopRightRadius = "10px";
+        }else if(i==size*size-size+1){
+            square.style.borderBottomLeftRadius = "10px";
+        }else if(i==size*size){
+            square.style.borderBottomRightRadius = "10px";
+        }
     }
  
 }
@@ -23,17 +37,30 @@ function buildGrid(size){
 //this function paints the grid
 function paintGrid(elem, color){
     let square = elem.target;    
-    square.style.backgroundColor = color
+    square.style.backgroundColor = color;
 }
 
-function eraseGrid(){
-
+function eraseGrid(){   
+    grid.innerHTML = '';
+    buildGrid(gridSize);
 }
 
-console.log(grid.offsetHeight)
-grid.addEventListener('mousemove', e=>{    
+function getRandomRgb() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
+
+grid.addEventListener('mouseover', e=>{    
     if(e.target.classList == "square-grid"){
-        paintGrid(e, COLOR_BLACK);
+        //paintGrid(e, COLOR_BLACK);
+        paintGrid(e, getRandomRgb());
     }    
 });
-buildGrid(gridSize);
+eraseButton.addEventListener('click', () =>{    
+    eraseGrid();
+
+});
+
